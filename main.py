ae7fcef2b27d8b54137cda1656a7680fa7340581
Michael Spicer <>
2021-04-30 09:33:04 +0000

    re-entered the code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 empty = 25
-new_patients = 5
+new_patients = 23
 
 if new_patients <= empty:
-  empty = empty - new_patients
-  print(f"{empty} rooms are empty.")
+    empty = empty - new_patients
+    print(f"{empty} rooms are empty")
